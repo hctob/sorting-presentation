b23d6c535360e401b43c814be08a889d18285dee
Dan Bottcher <dbottch1@binghamton.edu>
2020-04-23 16:33:29 -0400

    Randomized quicksort should run at comparable speeds to mergesort now

--- random_quicksort.cpp
+++ random_quicksort.cpp
@@ -5,11 +5,7 @@
 
 namespace RQS {
     int partition(int* arr, size_t start, size_t end) {
-        //choose randomized pivot point beween start and end
-        std::random_device dev;
-        std::mt19937 rng(dev());
-        std::uniform_int_distribution<std::mt19937::result_type> pv(start, end - 1);
-	    size_t pivot = arr[pv(rng)];
+	    size_t pivot = arr[end];
         size_t i = start - 1;
 
         //from the start index to the end index of the partition
@@ -23,11 +19,19 @@ namespace RQS {
         return i + 1;
     }
 
+    int random_partition(int* arr, size_t start, size_t end) {
+        srand(time(NULL));
+        int r = low + rand() % (high - low);
+        swap(arr[r], arr[high]); //swaps high pivot position with random one
+
+        return partition(arr, start, end);
+
+    }
 
     void quicksort(int* arr, size_t start, size_t end) {
         if(start < end) {
             //get pivot point for quicksort based on relative start and end points
-            int p = partition(arr, start, end);
+            int p = random_partition(arr, start, end);
             //std::cout << "P = " << p << std::endl;
             //perform quicksort on the left partition
             quicksort(arr, start, p - 1);
